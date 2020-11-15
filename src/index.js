7c5d2d59319ca3a977afab66cadb9f6133e956aa
Muhammad Awais <awaisshaikh94@gmail.com>
2020-11-15 16:33:54 +0500

    route issue after gh-pages build

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/dominus-freight-react-dashboard/admin" render={props => <AdminLayout {...props} />} />
-      <Redirect from="/dominus-freight-react-dashboard" to="dominus-freight-react-dashboard/admin/login" />
+      <Redirect from="/dominus-freight-react-dashboard" to="/dominus-freight-react-dashboard/admin/login" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
